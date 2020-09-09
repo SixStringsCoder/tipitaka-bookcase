8691ea95e9e7d1d83be3f7e08c26ed0d81ab0bef
Steve Hanlon <rshanlon@gmail.com>
2020-09-08 17:57:35 -0700

    Started adding components - added Header with CSS

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
   import TipitakaData from "./stores/tipitaka-store.js";
+  import Header from "./Header.svelte";
 
   let tipitakaData;
   TipitakaData.subscribe((items) => (tipitakaData = items));
 
-  console.log(tipitakaData);
+  $: console.log(tipitakaData);
 </script>
 
 <style>
 
 </style>
+
+<Header />
---
+++ src/Header.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+
+</script>
+
+<style>
+  header {
+    min-width: 100%;
+    /* background-color: #63500a; */
+    text-align: center;
+    position: fixed;
+    z-index: 10000;
+  }
+
+  header h1 {
+    font-size: 2.5rem;
+    margin: 0.8rem;
+    font-weight: 100;
+    /*   letter-spacing: 1px; */
+  }
+
+  span {
+    border-top: 1px solid black;
+    border-bottom: 1px solid black;
+    padding: 3px 10px;
+  }
+</style>
+
+<header>
+  <h1 id="tipitaka-header">
+    <span>The Tipi&#7789;aka Shelves</span>
+  </h1>
+</header>
