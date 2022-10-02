eddcd69cde36920da837a6dbdc6d21190160e557
SixStringsCoder <rshanlon@gmail.com>
2022-10-02 14:10:07 -0700

    2 files modified

--- src/Menu.svelte
+++ src/Menu.svelte
@@ -3,131 +3,29 @@
   export let selectedBooks: string;
 
   const categories = Object.keys(tipitakaData)
+  let topic = "Tipitaka"
+  $: topics = tipitakaData[topic]
+  $: console.log(topic, topics)
 </script>
 
 <div class="menu-container">
   <!-- svelte-ignore a11y-no-onchange -->
-  <select name="menu-1" class="menu">
+  <select name="menu-1" class="menu" bind:value={topic}>
     <option value="reset" disabled selected>Select Category</option>
     {#each categories as catgegory, i}
-      <option value={catgegory.toLowerCase()}>{catgegory}</option> 
+      <option value={catgegory}>{catgegory}</option> 
     {/each}  
   </select>
 </div>
 
 <div class="menu-container">
   <!-- svelte-ignore a11y-no-onchange -->
-  <select name="menu" class="menu" bind:value={selectedBooks} on:change>
+  <select name="menu-2" class="menu" bind:value={selectedBooks} on:change>
     <option value="reset" disabled selected>Learn More</option>
-    <optgroup label="Tipitaka">
-        <option value="vinaya">Vinaya Pi&#7789;aka</option>
-        <option value="suttanta">Suttanta Pi&#7789;aka</option>
-        <option value="abhidhamma">Abhidhamma Pi&#7789;aka</option>
-        <option value="digha-nikaya" data-basket="suttanta">
-          Long Discourses (Digha Nikāya)
-        </option>
-        <option value="majjhima-nikaya" data-basket="suttanta">
-          Medium-length Discourses (Majjhima Nikāya)
-        </option>
-        <option value="samyutta-nikaya" data-basket="suttanta">
-          Short Discourses/Connected Discourses or Kindred Sayings (Samyutta Nikāya)
-        </option>
-        <option value="anguttara-nikaya" data-basket="suttanta">
-          Numerical Discourses or Gradual Sayings (Aṅguttara Nikāya)
-        </option>
-        <option value="khuddaka-nikaya" data-basket="suttanta">
-          Minor Collection (Khuddaka Nikāya)
-        </option>
-      </optgroup>
-
-      <optgroup label="Well-known sections">
-        <option value="dhammapada" data-basket="suttanta">Dhammapada</option>
-        <option value="jataka" data-basket="suttanta">Jataka Tales</option>
-        <option value="patthana" data-basket="abhidhamma">Paṭṭhāna Matrix</option>
-        <option value="udana" data-basket="suttanta">Udāna</option>
-        <option value="itivuttaka" data-basket="suttanta">Itivuttaka</option>
-        <option value="khuddakapatha" data-basket="suttanta">Khuddakapāṭha</option>
-        <option value="maha-vagga-digha" data-basket="suttanta">
-          Wheel of Dependent Origination (Paṭiccasamuppāda)
-        </option>
-        <option value="maha-vagga-digha" data-basket="suttanta">
-          Mahāsatipaṭṭhāna Sutta (Digha)
-        </option>
-        <option value="mulapannasa" data-basket="suttanta">
-          Mahāsatipaṭṭhāna Sutta (Majjhima)
-        </option>
-        <option value="mahavagga" data-basket="vinaya">Pāṭimokkha</option>
-      </optgroup>
-
-      <optgroup label="From a 10-day course by S.N. Goenka">
-        <option value="sattaka" data-basket="suttanta">
-          Namo tassa bhagavato...
-        </option>
-        <option value="khuddakapatha" data-basket="suttanta">
-          Triple Gem and Precepts
-        </option>
-        <option value="pathika-vagga" data-basket="suttanta">
-          Day One - Āṭānāṭiya Sutta
-        </option>
-        <option value="khuddakapatha" data-basket="suttanta">
-          Day Two - Ratana Sutta
-        </option>
-        <option value="khuddakapatha" data-basket="suttanta">
-          Day Three - Karaṇīyametta Sutta (Mettasuttaṃ)
-        </option>
-        <option value="patthana" data-basket="suttanta">
-          Day Five - Tikapaṭṭhāna
-        </option>
-        <option value="maha-vagga-digha" data-basket="suttanta">
-          Day Six - Paṭiccasamuppāda (Chain of Conditioned Arising)
-        </option>
-        <option value="udana" data-basket="suttanta">
-          Day Six - Udāna - Buddhi Vagga (Enlightenment)
-        </option>
-        <option value="maha-vagga-samyutta" data-basket="suttanta">
-          Day Seven - Bojjhaṅgaparitta
-        </option>
-        <option value="jataka" data-basket="suttanta">
-          Day Eight - Mittānisaṃsa
-        </option>
-        <option value="khuddakapatha" data-basket="suttanta">
-          Day Nine - Maṅgala Suttaṃ
-        </option>
-        <option value="khuddakapatha" data-basket="suttanta">
-          Day Ten - Mettā-Bhāvanā
-        </option>
-        <option value="thera-gatha" data-basket="suttanta">
-          Aniccā vata saṅkhārā...  (Saṭṭhi Nipāta)
-        </option>
-      </optgroup>
-
-    <optgroup label="Commentaries">
-      <option value="commentaries" data-bookcase="bookcase">
-        Aṭṭhakathā (Commentaries) bookcase
-      </option>
-      <option value="path-of-purification">Visuddhi Magga</option>
-      <option value="Samantapasadika">Samantapasadika</option>
-      <option value="Kankhavitarani">Kankhavitarani</option>
-      <option value="Sumangalavilasini">Sumangalavilasini</option>
-      <option value="Papañcasudani">Papañcasudani</option>
-      <option value="Saratthappakasini">Saratthappakasini</option>
-      <option value="Manorathapurani">Manorathapurani</option>
-      <option value="Paramatthajotika">Paramatthajotika</option>
-    </optgroup>
-
-    <optgroup label="Sub-Commentaries">
-      <option value="sub-commentaries" data-bookcase="bookcase">
-        Tika (Sub-commentaries) bookcase
-      </option>
-      <option value="Paramatthamañjusa">Paramatthamañjusa</option>
-      <option value="Vajirabuddhi-tika">Vajirabuddhi-tika</option>
-      <option value="Saratthadipani">Saratthadipani</option>
-      <option value="Vimativinodani">Vimativinodani</option>
-      <option value="Vinayatthamañjusa">Vinayatthamañjusa</option>
-      <option value="Dighanikaya-tika">Dighanikaya-tika</option>
-      <option value="Majjhimanikaya-tika">Majjhimanikaya-tika</option>
-    </optgroup>
-
+        {#each topics as topic}
+          <option value={topic.id}
+                  data-basket={topic.basket}>{topic.name}</option>
+        {/each}
   </select>
 </div>
 
--- src/stores/menu-items.js
+++ src/stores/menu-items.js
@@ -1,123 +1,171 @@
 export const tipitakaData = {
-  "Tipitaka": [
+  Tipitaka: [
     {
-      id: "vinaya",
-      name: "Vinaya",
-      location: "vinaya"
+      id: 'vinaya',
+      name: 'Vinaya Piṭaka',
+      basket: 'vinaya',
     },
     {
-      id: "suttanta",
-      name: "Suttanta",
-      location: "suttanta"
+      id: 'suttanta',
+      name: 'Suttanta Piṭaka',
+      basket: 'suttanta',
     },
     {
-      id: "abhidhamma",
-      name: "Abhidhamma",
-      location: "abhidhamma"
+      id: 'abhidhamma',
+      name: 'Abhidhamma Piṭaka',
+      basket: 'abhidhamma',
     },
     {
-      id: "digha-nikaya",
-      name: "Long Discourses (Digha Nikāya)",
-      location: "suttanta"
+      id: 'digha-nikaya',
+      name: 'Long Discourses (Digha Nikāya)',
+      basket: 'suttanta',
     },
     {
-      id: "majjhima-nikaya",
-      name: "Medium-length Discourses (Majjhima Nikāya)",
-      location: "suttanta"
+      id: 'majjhima-nikaya',
+      name: 'Medium-length Discourses (Majjhima Nikāya)',
+      basket: 'suttanta',
     },
     {
-      id: "samyutta-nikaya",
-      name: "Short Discourses/Connected Discourses or Kindred Sayings (Samyutta Nikāya)",
-      location: "suttanta"
+      id: 'samyutta-nikaya',
+      name: 'Short Discourses/Connected Discourses or Kindred Sayings (Samyutta Nikāya)',
+      basket: 'suttanta',
     },
     {
-      id: "anguttara-nikaya",
-      name: "Numerical Discourses or Gradual Sayings (Aṅguttara Nikāya)",
-      location: "suttanta"
+      id: 'anguttara-nikaya',
+      name: 'Numerical Discourses or Gradual Sayings (Aṅguttara Nikāya)',
+      basket: 'suttanta',
     },
     {
-      id: "khuddaka-nikaya",
-      name: "Minor Collection (Khuddaka Nikāya)",
-      location: "suttanta"
-    }
+      id: 'khuddaka-nikaya',
+      name: 'Minor Collection (Khuddaka Nikāya)',
+      basket: 'suttanta',
+    },
   ],
-  "Well-known sections": [
-
+  'Well-known sections': [
+    {
+      id: 'dhammapada',
+      name: 'Dhammapada',
+      basket: 'suttanta',
+    },
+    {
+      id: 'jataka',
+      name: 'Jataka Tales',
+      basket: 'suttanta',
+    },
+    {
+      id: 'patthana',
+      name: 'Paṭṭhāna Matrix',
+      basket: 'abhidhamma',
+    },
+    {
+      id: 'udana',
+      name: 'Udāna',
+      basket: 'suttanta',
+    },
+    {
+      id: 'itivuttaka',
+      name: 'Itivuttaka',
+      basket: 'suttanta',
+    },
+    {
+      id: 'khuddakapatha',
+      name: 'Khuddakapāṭha',
+      basket: 'suttanta',
+    },
+    {
+      id: 'maha-vagga-digha',
+      name: 'Wheel of Dependent Origination (Paṭiccasamuppāda)',
+      basket: 'suttanta',
+    },
+    {
+      id: 'maha-vagga-digha',
+      name: 'Mahāsatipaṭṭhāna Sutta (Digha)',
+      basket: 'suttanta',
+    },
+    {
+      id: 'mulapannasa',
+      name: 'Mahāsatipaṭṭhāna Sutta (Majjhima)',
+      basket: 'suttanta',
+    },
+    {
+      id: 'mahavagga',
+      name: 'Pāṭimokkha',
+      basket: 'vinaya',
+    },
   ],
-  "From a 10-day course by S.N. Goenka": [
-
+  'From a 10-day course by S.N. Goenka': [
+    {
+      id: 'sattaka',
+      name: 'Namo tassa bhagavato...',
+      basket: 'suttanta',
+    },
+    {
+      id: 'khuddakapatha',
+      name: 'Triple Gem and Precepts',
+      basket: 'suttanta',
+    },
+    {
+      id: 'pathika-vagga',
+      name: 'Day One - Āṭānāṭiya Sutta',
+      basket: 'suttanta',
+    },
+    {
+      id: 'khuddakapatha',
+      name: 'Day Two - Ratana Sutta',
+      basket: 'suttanta',
+    },
+    {
+      id: 'khuddakapatha',
+      name: 'Day Three - Karaṇīyametta Sutta (Mettasuttaṃ)',
+      basket: 'suttanta',
+    },
+    {
+      id: 'patthana',
+      name: 'Day Five - Tikapaṭṭhāna',
+      basket: 'suttanta',
+    },
+    {
+      id: 'maha-vagga-digha',
+      name: 'Day Six - Paṭiccasamuppāda (Chain of Conditioned Arising)',
+      basket: 'suttanta',
+    },
+    {
+      id: 'udana',
+      name: 'Day Six - Udāna - Buddhi Vagga (Enlightenment)',
+      basket: 'suttanta',
+    },
+    {
+      id: 'maha-vagga-samyutta',
+      name: 'Day Seven - Bojjhaṅgaparitta',
+      basket: 'suttanta',
+    },
+    {
+      id: 'jataka',
+      name: 'Day Eight - Mittānisaṃsa',
+      basket: 'suttanta',
+    },
+    {
+      id: 'khuddakapatha',
+      name: 'Day Nine - Maṅgala Suttaṃ',
+      basket: 'suttanta',
+    },
+    {
+      id: 'khuddakapatha',
+      name: 'Day Ten - Mettā-Bhāvanā',
+      basket: 'suttanta',
+    },
+    {
+      id: 'thera-gatha',
+      name: 'Aniccā vata saṅkhārā...  (Saṭṭhi Nipāta)',
+      basket: 'suttanta',
+    },
   ],
-  "Commentaries (Aṭṭhakathā)": [
+  'Commentaries (Aṭṭhakathā)': [],
+  'Sub-Commentaries (Ṭīkā)': [],
+};
 
-  ],
-  "Sub-Commentaries (Ṭīkā)": [
-
-  ]
-}
-
-
-
-{/* 
-      <optgroup label="Well-known sections">
-        <option value="dhammapada" data-basket="suttanta">Dhammapada</option>
-        <option value="jataka" data-basket="suttanta">Jataka Tales</option>
-        <option value="patthana" data-basket="abhidhamma">Paṭṭhāna Matrix</option>
-        <option value="udana" data-basket="suttanta">Udāna</option>
-        <option value="itivuttaka" data-basket="suttanta">Itivuttaka</option>
-        <option value="khuddakapatha" data-basket="suttanta">Khuddakapāṭha</option>
-        <option value="maha-vagga-digha" data-basket="suttanta">
-          Wheel of Dependent Origination (Paṭiccasamuppāda)
-        </option>
-        <option value="maha-vagga-digha" data-basket="suttanta">
-          Mahāsatipaṭṭhāna Sutta (Digha)
-        </option>
-        <option value="mulapannasa" data-basket="suttanta">
-          Mahāsatipaṭṭhāna Sutta (Majjhima)
-        </option>
-        <option value="mahavagga" data-basket="vinaya">Pāṭimokkha</option>
-      </optgroup>
-
-      <optgroup label="From a 10-day course by S.N. Goenka">
-        <option value="sattaka" data-basket="suttanta">
-          Namo tassa bhagavato...
-        </option>
-        <option value="khuddakapatha" data-basket="suttanta">
-          Triple Gem and Precepts
-        </option>
-        <option value="pathika-vagga" data-basket="suttanta">
-          Day One - Āṭānāṭiya Sutta
-        </option>
-        <option value="khuddakapatha" data-basket="suttanta">
-          Day Two - Ratana Sutta
-        </option>
-        <option value="khuddakapatha" data-basket="suttanta">
-          Day Three - Karaṇīyametta Sutta (Mettasuttaṃ)
-        </option>
-        <option value="patthana" data-basket="suttanta">
-          Day Five - Tikapaṭṭhāna
-        </option>
-        <option value="maha-vagga-digha" data-basket="suttanta">
-          Day Six - Paṭiccasamuppāda (Chain of Conditioned Arising)
-        </option>
-        <option value="udana" data-basket="suttanta">
-          Day Six - Udāna - Buddhi Vagga (Enlightenment)
-        </option>
-        <option value="maha-vagga-samyutta" data-basket="suttanta">
-          Day Seven - Bojjhaṅgaparitta
-        </option>
-        <option value="jataka" data-basket="suttanta">
-          Day Eight - Mittānisaṃsa
-        </option>
-        <option value="khuddakapatha" data-basket="suttanta">
-          Day Nine - Maṅgala Suttaṃ
-        </option>
-        <option value="khuddakapatha" data-basket="suttanta">
-          Day Ten - Mettā-Bhāvanā
-        </option>
-        <option value="thera-gatha" data-basket="suttanta">
-          Aniccā vata saṅkhārā...  (Saṭṭhi Nipāta)
-        </option>
-      </optgroup>
+{
+  /* 
 
     <optgroup label="Commentaries">
       <option value="commentaries" data-bookcase="bookcase">
@@ -144,4 +192,5 @@ export const tipitakaData = {
       <option value="Vinayatthamañjusa">Vinayatthamañjusa</option>
       <option value="Dighanikaya-tika">Dighanikaya-tika</option>
       <option value="Majjhimanikaya-tika">Majjhimanikaya-tika</option>
-    </optgroup> */}
+    </optgroup> */
+}
